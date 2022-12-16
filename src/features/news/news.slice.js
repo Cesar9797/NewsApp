8b774d84e96d82d207e1f7b6b71ca45685c10a5e
Cesar9797 <cesar.lararaya@gmail.com>
2022-12-16 10:41:43 -0600

    Actualizando fecha de endpoint a 16122022

--- src/features/news/news.slice.js
+++ src/features/news/news.slice.js
@@ -37,7 +37,7 @@ export const { setNews, updateNews } = newsSlice.actions;
 
 export const setNewsThunk = () => (dispatch) => {
   axios.get(
-    "https://newsapi.org/v2/everything?q=tesla&from=2022-11-15&sortBy=publishedAt&apiKey=3f5daa84d6a54f24b8b1d1450be963f5"
+    "https://newsapi.org/v2/everything?q=tesla&from=2022-11-16&sortBy=publishedAt&apiKey=3f5daa84d6a54f24b8b1d1450be963f5"
   ).then(res => dispatch(setNews(res.data.articles)));
 };
 
